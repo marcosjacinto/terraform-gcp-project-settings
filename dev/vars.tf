fa85ffc5a5ee6bdb3e84b839bc0a5edac2a07223
marcos <marcos.gomesjacinto@gmail.com>
2022-10-17 15:18:27 -0300

    dev environment

---
+++ dev/vars.tf
@@ -0,0 +1,16 @@
+variable "project_id" {
+  description = "Project name"
+  type        = string
+}
+
+variable "region" {
+  description = "Default compute region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "Default compute zone"
+  type        = string
+  default     = "us-central1-a"
+}
