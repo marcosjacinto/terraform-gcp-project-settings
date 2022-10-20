ab3197148f779aab031875be87ad2dcb0690fe8f
marcos <marcos.gomesjacinto@gmail.com>
2022-10-20 11:59:09 -0300

    rewriting threshold rules

--- modules/project_setup/billing.tf
+++ modules/project_setup/billing.tf
@@ -10,9 +10,24 @@ resource "google_billing_budget" "standard_budget" {
         units = var.billing_budget_amount
       }
     }
-    dynamic "threshold_rules" {
-      for_each = toset([0.25, 0.5, 0.75, 0.9, 1.0])
-      threshold_percent = each.key
+    # dynamic "threshold_rules" {
+    #   for_each = toset([0.25, 0.5, 0.75, 0.9, 1.0])
+    #   threshold_percent = each.key
+    # }
+    threshold_rules {
+      threshold_percent = 0.25
+    }
+    threshold_rules {
+      threshold_percent = 0.50
+    }
+    threshold_rules {
+      threshold_percent = 0.75
+    }
+    threshold_rules {
+      threshold_percent = 0.90
+    }
+    threshold_rules {
+      threshold_percent = 1.00
     }
     depends_on = [
       time_sleep.wait_30_seconds
